ecdcaff3c6fb3a808d239a0793940e68db21ef74
tcallred <taylor.allred21@gmail.com>
2018-10-09 09:12:29 -0600

    Starting blank

---
+++ screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
+export default class HomeScreen extends React.Component {
+
+    render() {
+        return (
+            <View>
+                {
+                    //TODO Fill me in
+                }
+            </View>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+
+});
